1eac0f01a0f4a748c1a4bfb8ecd008f5a4fb8d6e
Maxine Mason <maxmason2000@gmail.com>
2022-02-01 11:54:32 -0500

    N can be odd.

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ const sss = new Shamir();
 
 const S = 123456789; // secret
 const N = 6; // parts to divide secret into
-const K = N - Math.floor(Math.random()*N/2); // subset of parts sufficient to recover secret
+const K = N - Math.floor(Math.random()*N/2); // random subset of parts sufficient to recover secret
 const P = 2 ** 127 - 1; // 12th Mersenne Prime; P>S>N
 
 // forming the polynomial of degree (K-1):
